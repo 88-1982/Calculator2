77f4317b8523342fe9733800b38adbd50c99f060
De Aunte Hall <deauntehall@Des-MacBook-Pro.local>
2021-01-11 21:23:24 -0800

    add function sub and alert sub

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,16 @@ const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
 
 alert (add(number1,number2));
+
+function sub(number1, number2) {
+  return number1 + number2;
+}
+
+const number1 = parseInt(prompt("Enter a number:"));
+const number2 = parseInt(prompt("Enter another number:"));
+
+alert (sub(number1,number2));
+
+
+
+
